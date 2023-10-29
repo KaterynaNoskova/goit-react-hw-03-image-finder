1f2aca78502c3e7fdacaba46817d19f5ada57a52
KaterynaNoskova <kateryna.noskova@gmail.com>
2023-10-29 22:21:46 +0100

    fixed index 2.0

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,17 +5,6 @@ import css from './Modal.module.css';
 const modalRoot = document.querySelector('#root');
 
 export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.escKeyDown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.escKeyDown);
-  }
-  escKeyDown = evt => {
-    if (evt.code === 'Escape') {
-      this.props.onClick();
-    }
-  };
   backdropClick = evt => {
     if (evt.target === evt.currentTarget) {
       this.toggleModal();
